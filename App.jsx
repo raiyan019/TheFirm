4de77d93d6fb5237a46f87378407231ec1f1e72c
Raiyan Ahmed <raiyan00719@gmail.com>
2025-02-05 23:43:51 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-yellow_color">App</div>
+  )
+};
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+ 
 export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}" // Ensures Tailwind scans these files for class usage
+    ],
+    theme: {
+      extend: {
+        colors: {
+            yellow_color: "var(--yellow_color)",
+            text_black_color: "var(--text_black_color)",
+            bg_gray_one: "var(--bg_gray_one)",
+            bg_gray_two: "var(--bg_gray_two)",
+            bg_white_one: "var(--bg_white_one)",
+            bg_white: "var(--bg_white)",
+        },
+        fontFamily: {
+          sans: ["Inter", "sans-serif"]
+        }
+      }
+    },
+    plugins: []
+  };
